94be5d8eafa79f2b4ef78ab57643ccf616456822
Mittal Soni <mittalsoni212@gmail.com>
2025-04-08 15:12:42 +0530

    committing the updated ApiReaderNewApplication

--- src/main/java/com/example/api_reader_new/ApiReaderNewApplication.java
+++ src/main/java/com/example/api_reader_new/ApiReaderNewApplication.java
@@ -2,20 +2,40 @@ package com.example.api_reader_new;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-//import javax.servlet.MultipartConfigElement;
 
+//@SpringBootApplication // Now scans the entire com.example.api_reader_new.*
+//@ComponentScan(basePackages = {"com.example.pdfparser"})
+@SpringBootApplication(scanBasePackages = "com.example.pdfparser")
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example.pdfparser.controller", "com.example.pdfparser.service"})
 public class ApiReaderNewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiReaderNewApplication.class, args);
     }
+}
+
+
+
+
+
+//package com.example.api_reader_new;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+////import javax.servlet.MultipartConfigElement;
+//
+//
+//@SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.pdfparser.controller", "com.example.pdfparser.service"})
+//public class ApiReaderNewApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApiReaderNewApplication.class, args);
+//    }
 //    @Bean
 //    public MultipartConfigElement multipartConfigElement() {
 //        return new MultipartConfigElement("");
 //    }
-}
+
